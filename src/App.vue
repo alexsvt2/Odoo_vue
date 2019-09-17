dab15fabc1874c82969fc5ca5b743bfea65d6f79
Alexis López García <alexislopezgarcia@MacBook-Pro-de-Kimberly.local>
2019-09-17 08:16:44 -0700

    Modificaciones en App, Stock Inventory y Product List, codigo en comentarios eliminado

--- src/App.vue
+++ src/App.vue
@@ -5,35 +5,28 @@
         <v-btn text to="/">Home</v-btn>
         <v-btn text to="/about">About</v-btn>
         <v-btn text to="/product_list">Product List</v-btn>
+        <v-btn text to="/stock_inventory">Stock Inventory</v-btn>
       </v-toolbar-items>
-
+ 
       <v-spacer></v-spacer>
       <v-toolbar-title class="headline text-uppercase">
-        <!-- <span>Vuetify</span> -->
-        <span class="font-weight-light">MATERIAL DESIGN</span>
+        <span class="font-weight-light">Vue App</span>
       </v-toolbar-title>
     </v-app-bar>
-    <v-content>
-      <router-view />
+    <v-content style="margin-top: 10px">
+      <router-view/>
     </v-content>
-
-    
   </v-app>
 </template>
-
+ 
 <script>
 import HelloWorld from "./components/HelloWorld";
-
+ 
 export default {
   name: "App",
   components: {
     HelloWorld
   },
-  data: () => ({
-
-
-
-
-  })
+  data: () => ({})
 };
 </script>
--- src/views/StockInventory.vue
+++ src/views/StockInventory.vue
@@ -1,23 +1,26 @@
 <template>
-  <v-card class="mx-auto" max-width="400" tile>
-    <v-list-item v-for="item in stockInventories" three-line>
-      <v-list-item-content>
-        <v-list-item-title>{{item.name}}</v-list-item-title>
-        <v-list-item-subtitle>{{item.date}}</v-list-item-subtitle>
-        <v-list-item-subtitle>{{item.state}}</v-list-item-subtitle>
-      </v-list-item-content>
-      <v-list-item-action>
-        <v-btn icon>
-          <v-icon @click="goToStockInventoryDetail(item.id)" color="grey lighten-1">mdi-launch</v-icon>
-        </v-btn>
-      </v-list-item-action>
-    </v-list-item>
-  </v-card>
+  <div>
+    <v-simple-table>
+      <tbody>
+        <tr v-for="(item, index) in stockInventories" :key="index">
+          <td>{{ item.name }}</td>
+          <td>{{ item.date }}</td>
+          <td>{{ item.state}}</td>
+          <td>
+            <div class="text-center">
+              <v-btn text small color="primary" @click="goToStockInventoryDetail(item.id)">
+                <v-icon>open_in_new</v-icon>
+              </v-btn>
+            </div>
+          </td>
+        </tr>
+      </tbody>
+    </v-simple-table>
+  </div>
 </template>
-
 <script>
 import axios from "axios";
-
+ 
 export default {
   data() {
     return {
@@ -33,22 +36,17 @@ export default {
     },
     stockInventoryList() {
       axios
-        .get(`http://localhost:3000/stock-inventory?offset=0&limit=10`)
+        .get(`http://192.168.100.59:3000/stock-inventory?offset=0&limit=10`)
         .then(response => {
+          console.log("This is the response");
           this.stockInventories = response.data;
         })
         .catch(e => {
           this.errors.push(e);
         });
-      // .then(response => {
-      //   this.products = response.data;
-      // })
-      // .catch(e => {
-      //   this.errors.push(e);
-      // });
     }
   }
 };
 </script>
-
+ 
 <style lang="stylus"></style>
