243e9409bb4b219b26adf55c7ed7da41dce810fd
alg9105@gmail.com <alg9105@gmail.com>
2019-10-18 21:06:04 -0700

    Environment Api Url

--- src/environment.js
+++ src/environment.js
@@ -1,4 +1,6 @@
 export default {
   // apiURL: 'http://localhost:3000'
-  apiURL: 'http://192.168.100.59:3000'
+  // apiURL: 'http://192.168.100.59:3000'
+  // apiURL: 'http://microservices.nkodexsoft.com:3000'
+  apiURL: 'https://inventory-scan.nkodexsoft.com'
 };
