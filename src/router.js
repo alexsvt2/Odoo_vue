568f86d69082fb7e81f6f39e1829682018927a1b
Alexis Lopez <alg9105@gmail.com>
2019-10-25 21:48:33 -0700

    Elimine comentarios en el codigo

--- src/router.js
+++ src/router.js
@@ -15,70 +15,38 @@ Vue.use(Router);
 const router = new VueRouter({
   mode: 'history',
   base: process.env.BASE_URL,
-  routes: [{
-    path: '/',
-    name: 'login',
-    component: Login
-  },
-  {
-    path: '/home',
-    name: 'home',
-    component: Home,
-    meta: { requiresAuth: true }
-    // beforeEnter: (to, from, next) => {
-    //   console.log(to)
-    //   next('/');
-    // }
-  },
-  {
-    path: '/about',
-    name: 'about',
-    component: AboutView,
-    meta: { requiresAuth: true }
-  },
-  {
-    path: '/stock_inventory',
-    name: 'stockInventory',
-    component: StockInventory,
-    meta: { requiresAuth: true }
-  },
-  {
-    path: '/stock_inventory/product_list/:id',
-    name: 'product_list',
-    component: ProductList,
-    meta: { requiresAuth: true }
-  }
+  routes: [
+    {
+      path: '/',
+      name: 'login',
+      component: Login
+    },
+    {
+      path: '/home',
+      name: 'home',
+      component: Home,
+      meta: { requiresAuth: true }
+    },
+    {
+      path: '/about',
+      name: 'about',
+      component: AboutView,
+      meta: { requiresAuth: true }
+    },
+    {
+      path: '/stock_inventory',
+      name: 'stockInventory',
+      component: StockInventory,
+      meta: { requiresAuth: true }
+    },
+    {
+      path: '/stock_inventory/product_list/:id',
+      name: 'product_list',
+      component: ProductList,
+      meta: { requiresAuth: true }
+    }
   ]
 });
-
-// router.beforeEach((to, from, next) => {
-//   // debugger;
-//   if (usuario_logeado) {
-//     console.log('El usuario esta logeado');
-//     if (
-//       to.name === 'home' ||
-//       to.name === 'about' ||
-//       to.name === 'stockInventory'
-//     ) {
-//       console.log('El usuario es redirigido');
-//       next('/home');
-//     }
-//   } else {
-//     next('/');
-//   }
-//   next();
-// });
-// var currentUser = false;
-
-// router.beforeEach((to, from, next) => {
-//   var requiresAuth = to.matched.some(record => record.meta.requiresAuth);
-//   if (requiresAuth && loggedUser) {
-//     next({ path: '/home' }); // Autenticacion requerida Login
-//   } else if (loggedUser === false) {
-//     next({ path: '/' });
-//   }
-//   next();
-// });
 
 router.beforeEach((to, from, next) => {
   var requiresAuth = to.matched.some(record => record.meta.requiresAuth);
